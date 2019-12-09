e7d4883e03cde0da727f0bc05e4b4ab5c4e345d4
Angelo Antonio Salatino <angelosalatino@users.noreply.github.com>
2019-12-09 18:03:44 +0000

    the classifier in batch mode uses the new settings. I also added the versioning functionality of the classifier

--- classifier/classifier.py
+++ classifier/classifier.py
@@ -9,6 +9,7 @@ from classifier.ontology import Ontology as CSO
 from classifier.model import Model as MODEL
 from classifier.paper import Paper
 from classifier.result import Result
+from classifier.config import Config
 
 
 def run_cso_classifier(paper, modules="both", enhancement="first"):
@@ -51,19 +52,89 @@ def run_cso_classifier(paper, modules="both", enhancement="first"):
     t_paper = Paper(paper)
     result = Result()
 
-    # Passing parameters to the two classes (synt and sema)
-    synt_module = synt(cso, t_paper)
-    sema_module = sema(model, cso, t_paper)
+    # Passing parameters to the two classes (synt and sema) and actioning classifiers
 
     if modules == 'syntactic' or modules == 'both':
+        synt_module = synt(cso, t_paper)
         result.set_syntactic(synt_module.classify_syntactic())
     if modules == 'semantic' or modules == 'both':
+        sema_module = sema(model, cso, t_paper)
         result.set_semantic(sema_module.classify_semantic())
        
     result.set_enhanced(cso.climb_ontology(getattr(result, "union"), enhancement))
 
 
     return result.get_dict()
+
+
+def run_cso_classifier_batch_model_single_worker(papers, modules="both", enhancement="first"):
+    """Run the CSO Classifier in *BATCH MODE*.
+
+    It takes as input a set of papers, which include abstract, title, and keywords and for each one of them returns a
+    list of relevant concepts from CSO. This function requires a dictionary of papers, with each id corresponding to
+    the metadata of a paper, and few flags:
+    (i) modules, determines whether to run only the syntactic module, or the semantic module, or both;
+    (ii) enhancement, controls whether the classifier should infer super-topics, i.e., their first direct
+    super-topics or the whole set of topics up until root.
+    
+
+    Args:
+        papers (dictionary): contains the metadata of the papers, e.g., for each paper, there is title, abstract and
+        keywords {"id1":{"title": "","abstract": "","keywords": ""},"id2":{"title": "","abstract": "","keywords": ""}}.
+        modules (string): either "syntactic", "semantic" or "both" to determine which modules to use when
+        classifying. "syntactic" enables only the syntatcic module. "semantic" enables only the semantic module.
+        Finally, with "both" the classifier takes advantage of both the syntactic and semantic modules. Default =
+        "both".
+        enhancement (string): either "first", "all" or "no". With "first" the CSO classifier returns only the topics
+        one level above. With "all" it returns all topics above the resulting topics. With "no" the CSO Classifier
+        does not provide any enhancement.
+
+    Returns:
+        fcso (dictionary): contains the CSO Ontology.
+        fmodel (dictionary): contains a cache of the model, i.e., each token is linked to the corresponding CSO topic.
+    """
+
+    if modules not in ["syntactic", "semantic", "both"]:
+        raise ValueError("Error: Field modules must be 'syntactic', 'semantic' or 'both'")
+
+    if enhancement not in ["first", "all", "no"]:
+        raise ValueError("Error: Field enhances must be 'first', 'all' or 'no'")
+
+    # Loading ontology and model
+    cso = CSO()
+    model = MODEL()
+    paper = Paper()
+    
+    
+
+    # Passing parameters to the two classes (synt and sema)
+    synt_module = synt(cso)
+    sema_module = sema(model, cso)
+
+    # initializing variable that will contain output
+    class_res = dict()
+
+    for paper_id, paper_value in papers.items():
+        print("Processing:", paper_id)
+
+        paper.set_paper(paper_value)
+        result = Result()
+
+        # Passing paper and actioning the classifier    
+        if modules == 'syntactic' or modules == 'both':
+            synt_module.set_paper(paper)
+            result.set_syntactic(synt_module.classify_syntactic())
+        if modules == 'semantic' or modules == 'both':
+            sema_module.set_paper(paper)
+            result.set_semantic(sema_module.classify_semantic())
+           
+        result.set_enhanced(cso.climb_ontology(getattr(result, "union"), enhancement))
+        
+        class_res[paper_id] = result.get_dict()
+
+    return class_res
+
+
 
 
 def run_cso_classifier_batch_mode(papers, workers=1, modules="both", enhancement="first"):
@@ -117,85 +188,55 @@ def run_cso_classifier_batch_mode(papers, workers=1, modules="both", enhancement
     return class_res
 
 
-def run_cso_classifier_batch_model_single_worker(papers, modules="both", enhancement="first"):
-    """Run the CSO Classifier in *BATCH MODE*.
 
-    It takes as input a set of papers, which include abstract, title, and keywords and for each one of them returns a
-    list of relevant concepts from CSO. This function requires a dictionary of papers, with each id corresponding to
-    the metadata of a paper, and few flags:
-    (i) modules, determines whether to run only the syntactic module, or the semantic module, or both;
-    (ii) enhancement, controls whether the classifier should infer super-topics, i.e., their first direct
-    super-topics or the whole set of topics up until root.
+def setup():
+    import os
+    os.system("python -m spacy download en_core_web_sm")
+    cso = CSO()
+    model = MODEL()
     
+def update():
+    print("to be developed")
+    
+def version():
+    config = Config()
 
-    Args:
-        papers (dictionary): contains the metadata of the papers, e.g., for each paper, there is title, abstract and
-        keywords {"id1":{"title": "","abstract": "","keywords": ""},"id2":{"title": "","abstract": "","keywords": ""}}.
-        modules (string): either "syntactic", "semantic" or "both" to determine which modules to use when
-        classifying. "syntactic" enables only the syntatcic module. "semantic" enables only the semantic module.
-        Finally, with "both" the classifier takes advantage of both the syntactic and semantic modules. Default =
-        "both".
-        enhancement (string): either "first", "all" or "no". With "first" the CSO classifier returns only the topics
-        one level above. With "all" it returns all topics above the resulting topics. With "no" the CSO Classifier
-        does not provide any enhancement.
+    print("# ==============================")
+    print("#     CLASSIFIER")
+    print("# ==============================")
+    print("CSO Classifier version {}".format(config.get_classifier_version()))
 
-    Returns:
-        fcso (dictionary): contains the CSO Ontology.
-        fmodel (dictionary): contains a cache of the model, i.e., each token is linked to the corresponding CSO topic.
-    """
-
-    if modules not in ["syntactic", "semantic", "both"]:
-        raise ValueError("Error: Field modules must be 'syntactic', 'semantic' or 'both'")
-
-    if enhancement not in ["first", "all", "no"]:
-        raise ValueError("Error: Field enhances must be 'first', 'all' or 'no'")
-
-    # Loading ontology and model
-    cso, model = misc.load_ontology_and_chached_model()
-
-    # Passing parameters to the two classes (synt and sema)
-    synt_module = synt(cso)
-    sema_module = sema(model, cso)
-
-    # initializing variable that will contain output
-    class_res = dict()
-
-    for paper_id, paper_value in papers.items():
-        print("Processing:", paper_id)
-
-        # In this case we avoid computing other fields. We select only title, abstract and keywords
-        paper = dict()
-            
-        paper["title"] = paper_value["title"] if "title" in paper_value and not paper_value["title"] is None else ""
-        paper["abstract"] = paper_value["abstract"] if "abstract" in paper_value and not paper_value["abstract"] is None else ""
-        paper["keywords"] = paper_value["keywords"] if "keywords" in paper_value and not paper_value["keywords"] is None else ""
-        #just in case the value keywords contains an array of keywords
-        if isinstance(paper["keywords"], list):
-            paper["keywords"] = ', '.join(paper["keywords"])
-
-        class_res[paper_id] = dict()
-        class_res[paper_id]["syntactic"] = list()
-        class_res[paper_id]["semantic"] = list()
-        class_res[paper_id]["union"] = list()
-        class_res[paper_id]["enhanced"] = list()
-
-        if modules == 'syntactic' or modules == 'both':
-            synt_module.set_paper(paper)
-            class_res[paper_id]["syntactic"] = synt_module.classify_syntactic()
-        if modules == 'semantic' or modules == 'both':
-            sema_module.set_paper(paper)
-            class_res[paper_id]["semantic"] = sema_module.classify_semantic()
-
-        union = list(set(class_res[paper_id]["syntactic"] + class_res[paper_id]["semantic"]))
-        class_res[paper_id]["union"] = union
-
-        if enhancement == 'first':
-            enhanced = misc.climb_ontology(cso, union, "first")
-            class_res[paper_id]["enhanced"] = [x for x in enhanced if x not in union]
-        elif enhancement == 'all':
-            enhanced = misc.climb_ontology(cso, union, "all")
-            class_res[paper_id]["enhanced"] = [x for x in enhanced if x not in union]
-        elif enhancement == 'no':
-            pass
-
-    return class_res
+    import subprocess   
+    import sys
+    
+    latest_version = str(subprocess.run([sys.executable, '-m', 'pip', 'install', '{}==random'.format(config.get_package_name())], capture_output=True, text=True))
+    latest_version = latest_version[latest_version.find('(from versions:')+15:]
+    latest_version = latest_version[:latest_version.find(')')]
+    latest_version = latest_version.replace(' ','').split(',')[-1]
+    
+    if latest_version > config.get_classifier_version():
+        print("A more recent version ({}) of the CSO Classifier is available.".format(latest_version))
+        print("You can update this package by running: pip install cso-classifier -U")
+    elif latest_version == config.get_classifier_version():
+        print("The version of the CSO Classifier you are using is already up to date.")
+    elif latest_version < config.get_classifier_version():
+        print("The latest availble package is version {} and you are using version {}. There is an error in your configuration file.".format(latest_version,config.get_classifier_version()))
+    
+    print()
+    print("# ==============================")
+    print("#     ONTOLOGY")
+    print("# ==============================")
+    print("CSO ontology version {}".format(config.get_ontology_version()))
+    
+    import urllib.request, json  
+    with urllib.request.urlopen(config.get_cso_last_version_url()) as url:
+        data = json.loads(url.read().decode())
+        if data['last_version'] > config.get_ontology_version():
+            print("A more recent version ({}) of the Computer Science Ontology is available.".format(data['last_version']))
+            print("You can update this package by running the following instructions:")
+            print("1) import classifier.classifier as CSO")
+            print("2) CSO.update()")
+        elif data['last_version'] == config.get_ontology_version():
+            print("The version of the CSO Ontology you are using is already up to date.") 
+        elif data['last_version'] < config.get_ontology_version():
+            print("The latest availble package is version {} and you are using version {}. There is an error in your configuration file.".format(data['last_version'],config.get_ontology_version()))
