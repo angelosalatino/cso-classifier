0f4ce5d91cb7a826daa9f57116fd9d1bcdeb2717
Angelo Antonio Salatino <angelosalatino@users.noreply.github.com>
2021-04-23 16:26:40 +0200

    cleared text script

--- classifier/test.py
+++ classifier/test.py
@@ -1,12 +1,13 @@
-import classifier.classifier as classifier
 import json
 
+import classifier.classifier as classifier
 
 def test_classifier_single_paper():
     """ Functionality that tests the classifier with a single paper.
-    It loads two papers, it calls the classifier with certain parameters and then it prints the results over the console.
+    It loads two papers, it calls the classifier with certain parameters and then
+    it prints the results over the console.
     """
-        
+
     paper = {
         "title": "De-anonymizing Social Networks",
         "abstract": "Operators of online social networks are increasingly sharing potentially "
@@ -24,22 +25,22 @@ def test_classifier_single_paper():
         "keywords": "data mining, data privacy, graph theory, social networking (online)"
         }
 
-        
+
 
     print(paper["title"])
     print(paper["abstract"])
     print(paper["keywords"])
-        
+
     result = classifier.run_cso_classifier(paper, explanation = True)
-    
+
     print(json.dumps(result))
-    
-    
+
+
 def test_classifier_batch_mode():
     """ Functionality that tests the classifier in batch mode.
     It loads two papers, it calls the classifier with certain parameters and then it prints the results over the console.
     """
-        
+
     papers = dict()
     papers['paper1'] = {
         "title": "De-anonymizing Social Networks",
@@ -51,14 +52,14 @@ def test_classifier_batch_mode():
         "abstract": "The process of classifying scholarly outputs is crucial to ensure timely access to knowledge. However, this process is typically carried out manually by expert editors, leading to high costs and slow throughput. In this paper we present Smart Topic Miner (STM), a novel solution which uses semantic web technologies to classify scholarly publications on the basis of a very large automatically generated ontology of research areas. STM was developed to support the Springer Nature Computer Science editorial team in classifying proceedings in the LNCS family. It analyses in real time a set of publications provided by an editor and produces a structured set of topics and a number of Springer Nature Classification tags, which best characterise the given input. In this paper we present the architecture of the system and report on an evaluation study conducted with a team of Springer Nature editors. The results of the evaluation, which showed that STM classifies publications with a high degree of accuracy, are very encouraging and as a result we are currently discussing the required next steps to ensure large-scale deployment within the company.",
         "keywords": "Scholarly data, Ontology learning, Bibliographic data, Scholarly ontologies, Data mining, Conference proceedings Metadata"
         }
-    
-    
+
+
     for key, paper in papers.items():
         print(key)
         print(paper["title"])
         print(paper["abstract"])
         print(paper["keywords"])
-    
+
     results = classifier.run_cso_classifier_batch_mode(papers, workers = 2)
-    
+
     print(results)
