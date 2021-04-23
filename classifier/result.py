34ac28f510c6dca4140b3854fa8265bc2023e624
Angelo Antonio Salatino <angelosalatino@users.noreply.github.com>
2021-04-23 16:12:24 +0200

    refactored the result class

--- classifier/result.py
+++ classifier/result.py
@@ -1,6 +1,6 @@
 class Result:
     """ A simple abstraction layer for retrieving the results """
-    
+
     def __init__(self, explanation = False):
         """ Initialising the ontology class
         """
@@ -20,85 +20,84 @@ class Result:
         """ Function that fills the union object
         """
         self.union = list(set(self.syntactic + self.semantic))
-        
-    
+
+
     def get_dict(self):
         """ Returns a dictionary containing all relevant objects
         """
         return {attr: getattr(self, attr) for attr in self.result_attr}
-    
-    
+
+
     def set_syntactic(self, syntactic):
         """ Sets the syntactic variable
         """
         self.syntactic = syntactic
         self.merge()
-        
-        
+
+
     def get_syntactic(self):
         """ Gets the syntactic variable
         """
         return self.syntactic
-    
-        
+
+
     def set_semantic(self, semantic):
         """ Sets the semantic variable
         """
         self.semantic = semantic
         self.merge()
-        
-        
+
+
     def get_semantic(self):
         """ Gets the semantic variable
         """
         return self.semantic
-    
-    
+
+
     def set_union(self, union):
         """ Sets the union variable
         """
         self.union = union
-            
-        
+
+
     def get_union(self):
         """ Gets the syntactic variable
         """
         return self.union
-    
-        
+
+
     def set_enhanced(self, enhanced):
         """ Sets the enhanced variable
         """
         self.enhanced = [x for x in list(enhanced.keys()) if x not in self.union]
         self.complete_explanation(enhanced)
-        
-    
+
+
     def get_enhanced(self):
         """ Gets the enhanced variable
         """
         return self.enhanced
-    
-    
+
+
     def dump_temporary_explanation(self, temporary_explanation):
-        """ It dumps the temporary explanation. After it will be reorganised better for all topics (including the enhanced ones)
+        """ It dumps the temporary explanation. After it will be reorganised
+        better for all topics (including the enhanced ones)
         """
         if self.explanation_flag:
             for topic, chunks in temporary_explanation.items():
                 if topic not in self.explanation:
                     self.explanation[topic] = set()
                 self.explanation[topic] = self.explanation[topic].union(chunks)
-            
-                    
+
+
     def complete_explanation(self, enhanced):
         """ It creates the explanation also for the enhanced topics
         """
-        
+
         if self.explanation_flag:
             for enhanced_topic, value in enhanced.items():
                 if enhanced_topic not in self.explanation:
                     self.explanation[enhanced_topic] = set()
                 self.explanation[enhanced_topic] = self.explanation[enhanced_topic].union(*[self.explanation[topic] for topic in value['broader of'] if topic in self.explanation])
-        
+
             self.explanation = {topic: list(value) for topic, value in self.explanation.items()}
-                
-                
