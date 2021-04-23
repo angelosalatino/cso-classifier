a6c9535e5e838864c5809919510d7411035c7403
Angelo Antonio Salatino <angelosalatino@users.noreply.github.com>
2021-04-23 15:56:20 +0200

    cleaned misc script

--- classifier/misc.py
+++ classifier/misc.py
@@ -1,8 +1,8 @@
 import sys
-import requests
-from hurry.filesize import size
 from itertools import islice
 import os
+from hurry.filesize import size
+import requests
 
 
 def download_file(url, filename):
@@ -15,48 +15,48 @@ def download_file(url, filename):
     Returns:
         boolean: whether it is successful or not.
     """
-    with open(filename, 'wb') as f:
+    is_downloaded = False
+    with open(filename, 'wb') as file:
         response = requests.get(url, stream=True)
         total = response.headers.get('content-length')
 
         if total is None:
             #f.write(response.content)
             print('There was an error while downloading the new version of the ontology.')
-            return False
         else:
             downloaded = 0
             total = int(total)
             for data in response.iter_content(chunk_size=max(int(total/1000), 1024*1024)):
                 downloaded += len(data)
-                f.write(data)
+                file.write(data)
                 done = int(50*downloaded/total)
                 sys.stdout.write('\r[{}{}] {}/{}'.format('█' * done, '.' * (50-done), size(downloaded), size(total)))
                 sys.stdout.flush()
             sys.stdout.write('\n')
             print('[*] Done!')
-            return True
-    
-    return False
+            is_downloaded = True
+
+    return is_downloaded
 
 
 def chunks(data, size):
     """Yield successive n-sized chunks from l.
     """
     # https://stackoverflow.com/questions/22878743/how-to-split-dictionary-into-multiple-dictionaries-fast
-    it = iter(data)
+    iterator = iter(data)
     for i in range(0, len(data), size):
-        yield {k:data[k] for k in islice(it, size)}
-        
-        
+        yield {k:data[k] for k in islice(iterator, size)}
+
+
 def download_language_model(notification = True):
     """ Function for downloading the language model.
     """
     if notification:
         print_header("LANGUAGE MODEL")
-              
+
     os.system("python -m spacy download en_core_web_sm")
-    
-    
+
+
 def print_header(header):
     """ Printing header. Used when setting up, updating, versioning and so on.
     """
