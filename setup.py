85219856f79a9163bc89b8c7d52b16bd3c7bf41a
Angelo A. Salatino <angelosalatino@users.noreply.github.com>
2026-01-13 21:37:37 +0000

    updated the setup with new requirements, and more stable file reading

--- setup.py
+++ setup.py
@@ -1,14 +1,22 @@
+import io
+import os
 import setuptools
 import configparser
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+# --- Long description (robust UTF-8, fallback if README missing) ---
+readme = ""
+readme_path = os.path.join(os.path.dirname(__file__), "README.md")
+if os.path.exists(readme_path):
+    with io.open(readme_path, "r", encoding="utf-8") as fh:
+        readme = fh.read()
+else:
+    readme = "CSO Classifier: classify scientific documents with topics from the Computer Science Ontology."
+
 
 
 requirements_to_install = [
-    'igraph==0.10.4;python_version>="3.7"',
-    'python-igraph==0.9.1;python_version<"3.7"',
-    'gensim==3.8.3',
+    'igraph==0.10.4',
+    'gensim==4.3.3',
     'click==7.1.2',
     'hurry.filesize==0.9',
     'kneed==0.3.1',
@@ -22,10 +30,10 @@ requirements_to_install = [
 ]
 
 
-# Import version number from version.py
+# --- Version from config.ini (UTF-8 read) ---
 config = configparser.ConfigParser()
-config.read("cso_classifier/config.ini")
-__version__ = config['classifier']['classifier_version']
+config.read(os.path.join("cso_classifier", "config.ini"), encoding="utf-8")
+__version__ = config["classifier"]["classifier_version"]
 
 
 setuptools.setup(
@@ -34,7 +42,7 @@ setuptools.setup(
     author="Angelo Salatino",
     author_email="angelo.salatino@open.ac.uk",
     description="A light-weight Python app for classifying scientific documents with the topics from the Computer Science Ontology (https://cso.kmi.open.ac.uk/home).",
-    long_description=long_description,
+    long_description=readme,
     long_description_content_type="text/markdown",
     url="https://github.com/angelosalatino/cso-classifier",
     packages=['cso_classifier'],
@@ -45,5 +53,6 @@ setuptools.setup(
     ],
     package_data = {'cso_classifier' : ['assets/*','config.ini'] },
     install_requires=requirements_to_install,
-    python_requires='>=3.6.0',
+    license="Apache-2.0",
+    python_requires='>=3.11.0',
 )
